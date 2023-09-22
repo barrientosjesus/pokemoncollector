fc1614c010e8c10617a7ce9ea80ccc0241768598
barrientosjesus <jabarri1@gmail.com>
2023-09-22 15:02:08 -0700

    Add about, index, and home templates, views, and routes

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+pokemon = [
+    {'id': '0001', 'name': 'Bulbasaur', 'types': ['Grass', 'Poison']},
+    {'id': '0002', 'name': 'Ivysaur', 'types': ['Grass', 'Poison']},
+    {'id': '0008', 'name': 'Wartortle', 'types': ['Water']}
+]
+
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def pokemon_index(request):
+    return render(request, 'pokemon/pokemon_index.html', {
+        'pokemon': pokemon,
+    })
